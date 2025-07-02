fe6d497f6b8283447fc011c714b4d91cd185c69b
DinoRexGit <120120533+DinoRexHub@users.noreply.github.com>
2025-07-02 04:53:50 -0400

    Update extension.js

--- headphonepause@gnome/extension.js
+++ headphonepause@gnome/extension.js
@@ -1,8 +1,9 @@
+'use strict';
+
 const { GObject, St, GLib } = imports.gi;
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
-const MessageTray = imports.ui.messageTray;
 
 var HeadphonePauseExtension = GObject.registerClass(
 class HeadphonePauseExtension extends PanelMenu.Button {
@@ -15,57 +16,81 @@ class HeadphonePauseExtension extends PanelMenu.Button {
         });
         this.add_child(this.icon);
 
-        this._source = new MessageTray.Source("Headphone Pause", 'audio-headphones-symbolic');
-        Main.messageTray.add(this._source);
-
         this._toggle = new PopupMenu.PopupSwitchMenuItem("Pause audio on unplug", false);
         this._toggle.connect('toggled', (item, state) => {
             this._togglePause(state);
         });
         this.menu.addMenuItem(this._toggle);
-    }
 
-    _showNotification(title, body) {
-        let notification = new MessageTray.Notification(this._source, title, { body: body });
-        this._source.notify(notification);
+        this._process = null;
     }
 
     _togglePause(enabled) {
-        let action = enabled ? 'start' : 'stop';
-        let cmd = ['systemctl', '--user', action, 'headphone-pause.service'];
+        if (enabled) {
+            this._startScript();
+        } else {
+            this._stopScript();
+        }
+    }
+
+    _startScript() {
+        if (this._process)
+            return;  // Already running
 
         try {
-            let [res, pid] = GLib.spawn_async(null, cmd, null, GLib.SpawnFlags.DO_NOT_REAP_CHILD, null);
-            GLib.child_watch_add(GLib.PRIORITY_DEFAULT, pid, (pid, status) => {
-                if (status === 0)
-                    this._showNotification("Headphone Pause", enabled ? "Enabled" : "Disabled");
-                else
-                    this._showNotification("Headphone Pause", "Failed to " + action);
-            });
+            let [success, pid] = GLib.spawn_async(
+                null,
+                ['/home/adity/bin/headphone-pause.sh'],
+                null,
+                GLib.SpawnFlags.DO_NOT_REAP_CHILD,
+                null
+            );
+
+            if (success) {
+                this._process = pid;
+                GLib.child_watch_add(GLib.PRIORITY_DEFAULT, pid, (pid, status) => {
+                    this._process = null;
+                    this._toggle.setToggleState(false);
+                });
+            } else {
+                log('Failed to start headphone-pause script');
+                this._toggle.setToggleState(false);
+            }
         } catch (e) {
-            this._showNotification("Headphone Pause", "Error: " + e.message);
+            log('Exception starting headphone-pause script: ' + e.message);
+            this._toggle.setToggleState(false);
+        }
+    }
+
+    _stopScript() {
+        if (this._process) {
+            try {
+                GLib.spawn_command_line_sync(`kill ${this._process}`);
+                this._process = null;
+            } catch (e) {
+                log('Error killing headphone-pause script: ' + e.message);
+            }
         }
     }
 
     destroy() {
-        this.menu.removeAll();
-        //Main.messageTray.remove(this._source);
+        this._stopScript();
         super.destroy();
     }
 });
 
-let headphonePause;
+let headphonePauseExtension = null;
 
 function init() {}
 
 function enable() {
-    headphonePause = new HeadphonePauseExtension();
-    Main.panel.addToStatusArea('headphone-pause', headphonePause);
+    headphonePauseExtension = new HeadphonePauseExtension();
+    Main.panel.addToStatusArea('headphone-pause', headphonePauseExtension);
 }
 
 function disable() {
-    if (headphonePause) {
-        headphonePause.destroy();
-        headphonePause = null;
+    if (headphonePauseExtension) {
+        headphonePauseExtension.destroy();
+        headphonePauseExtension = null;
     }
 }
